0bf20ea73cebfd94e98f1822cd5f92d5243b44b8
RickCastroo <Heinrickoficial25@gmail.com>
2022-11-28 23:08:38 -0300

    V0.0.0.0

---
+++ Coleira-Azul-App/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dia #1 criando um app do zero!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'blue'
+  }
+});
